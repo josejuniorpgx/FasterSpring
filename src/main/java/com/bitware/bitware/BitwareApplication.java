5ebcc3da84ec527479e493dc54482bcc4d2cae4e
josejuniorpg <yunipgx@protonmail.com>
2024-11-07 18:41:53 -0600

    Base

---
+++ src/main/java/com/bitware/bitware/BitwareApplication.java
@@ -0,0 +1,13 @@
+package com.bitware.bitware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitwareApplication.class, args);
+	}
+
+}
